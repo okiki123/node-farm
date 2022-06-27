56e3b79366a8596d21db2713c06720d114e0c5a5
okiki123 <okikimalomo17@gmail.com>
2022-06-27 11:16:40 +0100

    Updated Commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+
+// Blocking Synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log(textIn);
@@ -8,4 +10,15 @@ const textOut = `This is what we know about the avocado: ${textIn}.`
 fs.writeFileSync('./txt/output.txt', textOut);
 
 console.log('File written');
+
+// Non-blocking Asynchronous way
+
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    if (err) return console.log('error')
+     console.log(data)
+
+     fs.writeFile('./txt/final.txt', `${data}`, err => {
+       console.log('File has been written');
+     })
+})
 
