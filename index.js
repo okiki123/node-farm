5e1132e430c9b15101f6bb2fbe4a50dcb6c21b2f
okiki123 <okikimalomo17@gmail.com>
2022-06-27 11:27:53 +0100

    Updated Commit

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const fs = require('fs');
-
+const http = require('http');
 
 // Blocking Synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.`
-fs.writeFileSync('./txt/output.txt', textOut);
+// const textOut = `This is what we know about the avocado: ${textIn}.`
+// fs.writeFileSync('./txt/output.txt', textOut);
 
-console.log('File written');
+// console.log('File written');
 
-// Non-blocking Asynchronous way
+// // Non-blocking Asynchronous way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    if (err) return console.log('error')
-     console.log(data)
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//     if (err) return console.log('error')
+//      console.log(data)
 
-     fs.writeFile('./txt/final.txt', `${data}`, err => {
-       console.log('File has been written');
-     })
+//      fs.writeFile('./txt/final.txt', `${data}`, err => {
+//        console.log('File has been written');
+//      })
+// })
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port 8000')
 })
 
